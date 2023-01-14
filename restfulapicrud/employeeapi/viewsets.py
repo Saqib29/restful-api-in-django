7f5487b93cf143a02d768f2dd16ab41031a88d4f
saqib <saqib29111@gmail.com>
2023-01-14 13:19:10 +0600

    serializer & viewsets

---
+++ restfulapicrud/employeeapi/viewsets.py
@@ -0,0 +1,9 @@
+from rest_framework import viewsets
+from . import models
+from . import serializers
+
+class EmployeeViewSet(viewsets.ModelViewSet):
+    queryset = models.Employee.all()
+    serializer_class = serializers.EmployeeSerializer
+
+# list(), retrieve(), create(), updtae(), destroy() provides this viewsets
